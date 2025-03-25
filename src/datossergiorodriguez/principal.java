bafd72598cdf63a7c984dd68be7b843405e0d9b8
SergioRodrigueeeeeeeezzzzzzzzz <srodriguezmayor@educa.madrid.org>
2025-03-25 20:46:28 +0100

    Nuevo mensaje

--- src/datossergiorodriguez/principal.java
+++ src/datossergiorodriguez/principal.java
@@ -4,5 +4,7 @@ package datossergiorodriguez;
 public class principal {
     public static void main(String[] args) {
         System.out.println("Nombre completo: Sergio Rodriguez");
+        System.out.println("Estoy haciendo esto a 20 minutos"
+        		+ "para que se acabe la entrega");
     }
 }
